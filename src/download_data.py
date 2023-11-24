e718499aba20172da74ff2e0ceb88da5b4d84cf5
Alistair Jones <101199560+alistair-jones@users.noreply.github.com>
2023-11-24 11:57:08 +0000

    Refactor and add plotting

--- src/download_data.py
+++ src/download_data.py
@@ -1,5 +1,7 @@
 from typing import Any, Literal, Callable
+
 import pandas as pd
+import plotly.express as px
 
 DATA_SOURCES = {
     # Available from: https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/
@@ -22,18 +24,34 @@ DATA_READERS = {
 }
 
 
-def get_data_reader(file_type: Literal["excel"]) -> Callable[[Any], pd.DataFrame]:
-    data_reader = DATA_READERS.get(file_type)
+def get_admissions_data() -> pd.DataFrame:
+    """Download the A&E admissions data as a pandas Dataframe. Performs 
+    minor preprocessing steps to select the required rows/columns from the 
+    excel sheet.
 
-    if data_reader: 
-        return data_reader
-    else:
-        raise NotImplementedError(f"File type '{file_type}' not supported")
+    Returns:
+        pd.DataFrame: A&E admissions data downloaded from the NHSE website.
+    """
+    activity_source = DATA_SOURCES["ae_activity"]
+    df_activity = (
+        _get_data_from_source(activity_source)
+        .pipe(_prep_raw_activity_data)
+    )
+
+    df_admissions = df_activity["Emergency Admissions"]
+
+    return df_admissions
 
 
-def get_data_from_source(source: dict[str, Any]) -> pd.DataFrame:
+def _prep_raw_activity_data(df_raw: pd.DataFrame) -> pd.DataFrame:
+    cols_to_drop = [col_name for col_name in df_raw.columns.get_level_values(0) if "Unnamed" in col_name]
+    df_pre = df_raw.drop(cols_to_drop, axis=1, level=0)
+    return df_pre
+
+
+def _get_data_from_source(source: dict[str, Any]) -> pd.DataFrame:
     io_type = source.get("io_type", "url")
-    read_data = get_data_reader(source.get("file_type"))
+    read_data = _get_data_reader(source.get("file_type"))
 
     if io_type == "url":
         df = read_data(source["io"], **source.get("loader_kwargs"))
@@ -43,24 +61,19 @@ def get_data_from_source(source: dict[str, Any]) -> pd.DataFrame:
     return df
 
 
-def prep_raw_activity_data(df_raw: pd.DataFrame) -> pd.DataFrame:
-    cols_to_drop = [col_name for col_name in df_raw.columns.get_level_values(0) if "Unnamed" in col_name]
-    df_pre = df_raw.drop(cols_to_drop, axis=1, level=0)
-    return df_pre
+def _get_data_reader(file_type: Literal["excel"]) -> Callable[[Any], pd.DataFrame]:
+    data_reader = DATA_READERS.get(file_type)
 
+    if data_reader: 
+        return data_reader
+    else:
+        raise NotImplementedError(f"File type '{file_type}' not supported")
 
-def get_admissions_data() -> pd.DataFrame:
-    activity_source = DATA_SOURCES["ae_activity"]
-    df_activity = (
-        get_data_from_source(activity_source)
-        .pipe(prep_raw_activity_data)
-    )
-
-    df_admissions = df_activity["Emergency Admissions"]
-
-    return df_admissions
 
 
 if __name__ == "__main__":
     df_admissions = get_admissions_data()
-    print(df_admissions)
+    print(df_admissions.head())
+
+    fig = px.line(df_admissions, y="Total Emergency Admissions")
+    fig.show()
